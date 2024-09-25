5590360ec50c17fe51b1c8a5ba009d85cbfca6a3
brucordeiro <brunapcordeiro7@gmail.com>
2024-09-25 10:20:17 -0300

    Insert na página de serviço

--- servicos_salvar.php
+++ servicos_salvar.php
@@ -23,7 +23,24 @@ and open the template in the editor.
 
         <form name="formServico" action="" method="post">
             <div id="body">
-
+            <?php 
+                //PASSO 1: Incluir as definições de acesso a dados
+                include "conexao_bd.php";
+                //PASSO 2:Capturar os valores digitados
+                $descricao = $_POST["txtDescricao"];
+                $preco = $_POST["txtPreco"];
+                //PASSO 3: Montar o comando SQL para inserir 
+                $sql = "INSERT INTO servico(descicao,preco) VALUES ('$descricao', '$preco')";
+                //PASSO 4:Executar o comando
+                if (executarComando($sql))
+                {
+                    echo "<h1>Servico adicionado com sucesso";
+                }
+                else
+                {
+                    echo "<h1>Não foi possivel cadastrar.</h1>";
+                }
+            ?>
                 
             </div>
         </form>
